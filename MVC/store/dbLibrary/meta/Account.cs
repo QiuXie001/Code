8ba99dd4cfb4aff2d27be071933eb2af893cd9d1
Qiu <QiuXie20240601@outlook.com>
2023-12-02 20:31:59 +0800

    meta文件补提交

---
+++ MVC/store/dbLibrary/meta/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db
+{
+    [MetadataType(typeof(metaClass))]
+    public partial class Account
+    {
+        public class metaClass
+        {
+            public int UserID { get; set; }
+            [Display(Name = "密码")]
+            [Required(ErrorMessage = "{0}必填")]
+            public string Password { get; set; }
+            [Display(Name = "年龄")]
+            [Range(16, 25)]
+            public string Age { get; set; }
+            [Display(Name = "邮箱")]
+            [DataType(DataType.EmailAddress)]
+            public string Email { get; set; }
+            [Display(Name = "身份证")]
+            [Required(ErrorMessage = "{0}必填")]
+            public string identityID { get; set; }
+            [Display(Name = "手机号")]
+            [Required(ErrorMessage = "{0}必填")]
+            [DataType(DataType.PhoneNumber, ErrorMessage = "请输入正确的手机号！")]
+            public string telephone { get; set; }
+        }
+    }
+}
---
+++ MVC/store/dbLibrary/meta/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db
+{
+    [MetadataType(typeof(metaClass))]
+    public partial class Books
+    {
+        public class metaClass
+        {
+            [Display(Name = "图书编号")]
+            public int BookId { get; set; }
+            [Display(Name = "作者")]
+            public string AuthorName { get; set; }
+            [Display(Name = "图书")]
+            public string Title { get; set; }
+            [Display(Name = "价格")]
+            public Nullable<decimal> Price { get; set; }
+            [Display(Name = "封面")]
+            public string BookCoverUrl { get; set; }
+            [Display(Name = "图书类别")]
+            public string BookType { get; set; }
+            [Display(Name = "标签")]
+            public string BookTag { get; set; }
+
+        }
+    }
+}
