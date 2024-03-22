89d57064ad1df761a1ca9115180dc0552b5e5f11
Qiu <QiuXie20240601@outlook.com>
2024-03-22 10:10:48 +0800

    #登录模块完成

--- JavaWebTest/demo/src/main/java/com/example/LoginServlet.java
+++ JavaWebTest/demo/src/main/java/com/example/LoginServlet.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,33 +18,60 @@ public class LoginServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
-        String html = "<form action=\"/demo/LoginServlet\" method=\"post\">"
-                + "userName:<input type=\"text\" name=\"name\"/><br>"
-                + "password:<input type=\"password\" name=\"pwd\"/><br>"
-                + "<input type=\"submit\" value=\"Login\"/></form>";
-        response.getWriter().println(html);
-        
+        // 设置响应代码
+        response.setContentType("text/html;charset=UTF-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        request.setAttribute("username", username);
+        request.setAttribute("password", password);
+        // 将获取的数据封装成对象，存储在request对象中,request对象中可以存储任何数据类型（
+        request.getRequestDispatcher("/Login.html").forward(request, response);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         // 获取表单提交的用户名和密码
         String username = request.getParameter("name");
         String password = request.getParameter("pwd");
+        boolean check = false;
+        // 加载配置文件
+        Properties properties = new Properties();
+        //ServletContext接口读取资源
+        InputStream stream = this.getServletContext().getResourceAsStream("/resources/static/document/12021051010.properties");
+        properties.load(new InputStreamReader(stream));
 
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html><html><head><title>Login</title></head><body>");
-        out.println("登陆成功！<br>");
-        out.println("Username: " + username + "<br>");
-        out.println("Password: " + password + "<br>");
-        out.println("</body></html>");
+        // 获取配置文件中的值
+        String UNameFrompProp = properties.getProperty("id");
+        String PWDFromProp = properties.getProperty("password");
+        System.out.println(UNameFrompProp + "/" + PWDFromProp);
+        System.out.println(username + "/" + password);
         
-        
-        response.setHeader("Refresh", "5;URL=/demo/page.html");
+        if (UNameFrompProp.equals(username) && PWDFromProp.equals(password)) {
+            check = true;
+        }
+
+        if (check) {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<!DOCTYPE html><html><head><title>Login</title></head><body>");
+            out.println("登陆成功！<br>");
+            out.println("Username: " + username + "<br>");
+            out.println("Password: " + password + "<br>");
+            out.println("</body></html>");
+            response.setHeader("Refresh", "5;URL=/demo/page.html");
+        }
+        else
+        {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<!DOCTYPE html><html><head><title>Login</title></head><body>");
+            out.println("登陆失败！<br>");
+            out.println("检查用户名或密码后重试！");
+            out.println("</body></html>");
+            response.setHeader("Refresh", "5;URL=/demo/LoginServlet");
+        }
     }
     
 }
